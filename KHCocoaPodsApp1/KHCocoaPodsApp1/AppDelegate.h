a48cee03217b6e870de784615152891269edc9d0
Kevin Hawkins <khawkins@salesforce.com>
2015-12-30 13:20:58 -0800

    Adding App project

---
+++ KHCocoaPodsApp1/KHCocoaPodsApp1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KHCocoaPodsApp1
+//
+//  Created by Kevin Hawkins on 12/30/15.
+//  Copyright © 2015 KH CocoaPods Tests. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KHCocoaPodsApp1/KHCocoaPodsApp1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KHCocoaPodsApp1
+//
+//  Created by Kevin Hawkins on 12/30/15.
+//  Copyright © 2015 KH CocoaPods Tests. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
